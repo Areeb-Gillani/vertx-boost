88107d557d34ad725b8848df7a397fb420efb94e
mawaisayub <muhammadawaisayub@gmail.com>
2025-01-12 16:32:07 +0500

    - Default value of repository annotation changed to "Primary".

--- src/main/java/io/github/areebgillani/boost/aspects/Repository.java
+++ src/main/java/io/github/areebgillani/boost/aspects/Repository.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Repository {
-    String value() default "";
+    String value() default "Primary";
 }
